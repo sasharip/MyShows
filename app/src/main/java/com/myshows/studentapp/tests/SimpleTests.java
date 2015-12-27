c5129f88c9d3c8f160731d6528aaf7d998dd1642
alex <vattghern777@gmail.com>
2015-12-27 19:21:23 +0200

    early tests

---
+++ app/src/main/java/com/myshows/studentapp/tests/SimpleTests.java
@@ -0,0 +1,13 @@
+package com.myshows.studentapp.tests;
+
+import android.widget.TextView;
+
+import com.myshows.studentapp.activities.MainActivity;
+
+public class SimpleTests extends MainActivity{
+
+    private SimpleTests msimpleTests;
+    private TextView mTextView;
+    
+
+}
